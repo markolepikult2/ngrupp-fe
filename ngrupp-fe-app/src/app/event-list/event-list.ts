fc27c3f5e80cc0dd6f1f6d5765e7ee9673cdd20e
marko <marko.lepikult@gmail.com>
2025-08-18 15:22:08 +0300

    Added "Book seat" button to event list.

--- ngrupp-fe-app/src/app/event-list/event-list.ts
+++ ngrupp-fe-app/src/app/event-list/event-list.ts
@@ -3,15 +3,17 @@ import { EventService } from '../event.service';
 import { Event } from '../models';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule} from '@angular/common/http';
+import {BookingDetailsComponent} from '../booking-details/booking-details';
 
 @Component({
   selector: 'app-event-list',
   templateUrl: './event-list.html',
-  imports: [CommonModule, HttpClientModule ],
+  imports: [CommonModule, HttpClientModule, BookingDetailsComponent],
   styleUrl: './event-list.scss'
 })
 export class EventList {
   events: Event[] = [];
+  selectedEventId: number | null = null;
 
   constructor(private eventService: EventService) {}
 
@@ -19,5 +21,10 @@ export class EventList {
     this.eventService.getEvents().subscribe((data) => {
       this.events = data;
     });
+  }
+
+  openBookingDetails(event: Event) {
+    console.log("Clicked Book Seat:", event);
+    this.selectedEventId = event.id;
   }
 }
