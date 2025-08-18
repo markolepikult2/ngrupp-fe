536267c541dbbb6ab03266ac13b640acf409865b
marko <marko.lepikult@gmail.com>
2025-08-18 10:34:08 +0300

    Cleanup obsolete component.

--- ngrupp-fe-app/src/app/header/header.component.ts
+++
@@ -1,9 +0,0 @@
-  import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-header',
-  templateUrl: './header.html',
-  styleUrl: './header.scss'
-})
-export class HeaderComponent {}
-
